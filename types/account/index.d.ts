6846dcc1b197ae58316e56b85f7428353c97e544
Carlos Knopel <carlos.knopel@gmail.com>
2023-07-31 14:15:06 -0400

    fix manual issues

---
+++ types/account/index.d.ts
@@ -0,0 +1,166 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import type { DarkReaderPropValues } from '../misc';
+import { DARK_READER_PROP_KEY } from '../../src/constants';
+import type { SHELL_APP_ID } from '../exports';
+import { StringOfLength } from '../../src/utils/typeUtils';
+
+export interface ZimletProp {
+	name: string;
+	zimlet: string;
+	_content: string;
+}
+
+export interface ZappDarkreaderModeZimletProp extends ZimletProp {
+	name: typeof DARK_READER_PROP_KEY;
+	zimlet: SHELL_APP_ID;
+	_content: DarkReaderPropValues;
+}
+
+export type SoapFetch = <Request, Response>(
+	api: string,
+	body: Request,
+	account?: string,
+	signal?: AbortSignal
+) => Promise<Response>;
+
+export type AccountState = {
+	authenticated: boolean;
+	account?: Account;
+	settings: AccountSettings;
+	zimbraVersion?: string;
+	usedQuota: number;
+};
+
+export type Account = {
+	id: string;
+	name: string;
+	displayName: string;
+	signatures: { signature: Array<unknown> };
+	identities: { identity: Array<{ id: string; name?: string; _attrs?: Partial<IdentityAttrs> }> };
+	rights: AccountRights;
+};
+
+export type DelegateProps = {
+	email: string;
+	right: string;
+};
+
+export type BooleanString = 'TRUE' | 'FALSE';
+
+type GenTimeObj = {
+	year: `${number}` & StringOfLength<4>;
+	month: `${number}` & StringOfLength<2>;
+	date: `${number}` & StringOfLength<2>;
+	hour: `${number}` & StringOfLength<2>;
+	min: `${number}` & StringOfLength<2>;
+	sec: `${number}` & StringOfLength<2>;
+	ms: (`.${number}` & StringOfLength<4>) | '';
+	timezone: 'Z' | '';
+};
+
+export type GeneralizedTime =
+	`${GenTimeObj['year']}${GenTimeObj['month']}${GenTimeObj['date']}${GenTimeObj['hour']}${GenTimeObj['min']}${GenTimeObj['sec']}${GenTimeObj['ms']}${GenTimeObj['timezone']}`;
+
+export type DurationUnit = 'd' | 'h' | 'm' | 's' | 'ms';
+
+export type Duration = `${number}${DurationUnit | ''}`;
+
+export interface AccountSettingsPrefs {
+	zimbraPrefOutOfOfficeExternalReply?: string;
+	zimbraPrefOutOfOfficeReply?: string;
+	zimbraPrefOutOfOfficeReplyEnabled?: BooleanString;
+	zimbraPrefOutOfOfficeExternalReplyEnabled?: BooleanString;
+	zimbraPrefExternalSendersType?: 'ALL' | 'ALLNOTINAB' | 'INAB' | 'INSD';
+	zimbraPrefOutOfOfficeSuppressExternalReply?: BooleanString;
+	zimbraPrefOutOfOfficeFreeBusyStatus?: 'BUSY' | 'OUTOFOFFICE';
+	zimbraPrefOutOfOfficeStatusAlertOnLogin?: BooleanString;
+	zimbraPrefIncludeSharedItemsInSearch?: BooleanString;
+	zimbraPrefIncludeSpamInSearch?: BooleanString;
+	zimbraPrefIncludeTrashInSearch?: BooleanString;
+	zimbraPrefOutOfOfficeFromDate?: GeneralizedTime;
+	zimbraPrefOutOfOfficeUntilDate?: GeneralizedTime;
+	zimbraPrefHtmlEditorDefaultFontColor?: string;
+	zimbraPrefHtmlEditorDefaultFontFamily?: string;
+	zimbraPrefHtmlEditorDefaultFontSize?: string;
+	zimbraPrefLocale?: string;
+	zimbraPrefMailPollingInterval?: Duration;
+	zimbraPrefMailTrustedSenderList?: Array<string> | string;
+	zimbraPrefTimeZoneId?: Array<string> | string;
+	[key: string]: string | number | Array<string | number>;
+}
+
+export type AccountSettingsAttrs = {
+	zimbraIdentityMaxNumEntries?: number;
+	[key: string]: string | number | Array<string | number>;
+};
+
+export type AccountSettings = {
+	attrs: AccountSettingsAttrs;
+	prefs: AccountSettingsPrefs;
+	props: Array<ZimletProp>;
+};
+
+export interface IdentityAttrs {
+	/** default mail signature for account/identity/dataSource */
+	zimbraPrefDefaultSignatureId?: string;
+	zimbraPrefForwardReplyFormat?: `'text' | 'html' | 'same'`;
+	/** forward/reply signature id for account/identity/dataSource */
+	zimbraPrefForwardReplySignatureId?: string;
+	/** email address to put in from header.  Deprecated on data source as of bug 67068. */
+	zimbraPrefFromAddress?: string;
+	/** Type of the email address from header. (sendAs or sendOnBehalfOf)  */
+	zimbraPrefFromAddressType: 'sendAs' | 'sendOnBehalfOf';
+	/** personal part of email address put in from header */
+	zimbraPrefFromDisplay?: string;
+	zimbraPrefIdentityId?: string;
+	/** name of the identity */
+	zimbraPrefIdentityName?: string;
+	zimbraPrefMailSignatureStyle?: 'outlook' | 'internet';
+	/** address to put in reply-to header */
+	zimbraPrefReplyToAddress?: string;
+	/** personal part of email address put in reply-to header */
+	zimbraPrefReplyToDisplay?: string;
+	/** TRUE if we should set a reply-to header */
+	zimbraPrefReplyToEnabled?: BooleanString;
+	/** name of folder to save sent mail in (deprecatedSince 5.0 in identity) */
+	zimbraPrefSentMailFolder?: string;
+	zimbraPrefWhenInFolderIds?: Array<string | null>;
+	/** TRUE if we should look at zimbraPrefWhenInFolderIds (deprecatedSince 5.0 in account) */
+	zimbraPrefWhenInFoldersEnabled?: BooleanString;
+	/** addresses that we will look at to see if we should use an identity (deprecatedSince 5.0 in account) */
+	zimbraPrefWhenSentToAddresses?: Array<string | null>;
+	/** TRUE if we should look at zimbraPrefWhenSentToAddresses (deprecatedSince 5.0 in account) */
+	zimbraPrefWhenSentToEnabled?: BooleanString;
+	/** whether or not to save outgoing mail (deprecatedSince 5.0 in identity) */
+	zimbraPrefSaveToSent?: BooleanString;
+}
+
+export type AccountRightTargetEmail = {
+	addr: string;
+};
+
+export type AccountRightTarget = {
+	d: string;
+	id: string;
+	name: string;
+	type: string;
+	email: Array<AccountRightTargetEmail>;
+};
+
+export type AccountRightName =
+	| 'sendAs'
+	| 'sendAsDistList'
+	| 'viewFreeBusy'
+	| 'sendOnBehalfOf'
+	| 'sendOnBehalfOfDistList';
+export type AccountRights = {
+	targets: Array<{
+		right: AccountRightName;
+		target: Array<AccountRightTarget>;
+	}>;
+};
---
+++ types/network/index.d.ts
@@ -0,0 +1,304 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import {
+	AccountRights,
+	AccountSettings,
+	AccountSettingsPrefs,
+	IdentityAttrs,
+	ZimletProp
+} from '../account';
+import { Tag } from '../tags';
+import { AccountACEInfo, Identity } from './entities';
+
+export * from './soap';
+
+export type ZimletPkgDescription = {
+	zimlet: Array<{
+		name: string;
+		label: string;
+		description: string;
+		version: string;
+		/* Property related to Zextras */ zapp?: 'true';
+		/* Property related to Zextras */ 'zapp-main'?: string;
+		/* Property related to Zextras */ 'zapp-version'?: string;
+		/* Property related to Zextras */ 'zapp-handlers'?: string;
+		/* Property related to Zextras */ 'zapp-style'?: string;
+		/* Property related to Zextras */ 'zapp-theme'?: string;
+		/* Property related to Zextras */ 'zapp-serviceworker-extension'?: string;
+	}>;
+	zimletContext: Array<{
+		baseUrl: string;
+		presence: 'enabled';
+		priority: number;
+	}>;
+};
+
+export type GetInfoResponse = {
+	name: string;
+	id: string;
+	attrs: {
+		_attrs: {
+			displayName: string;
+		};
+	};
+	prefs: {
+		_attrs: AccountSettingsPrefs;
+	};
+	signatures: {
+		signature: Array<any>;
+	};
+	identities: {
+		identity: Array<any>;
+	};
+	zimlets: {
+		zimlet: Array<ZimletPkgDescription>;
+	};
+	props: {
+		prop: Array<ZimletProp>;
+	};
+	version: string;
+	rights: AccountRights;
+};
+
+export type PropsMods = Record<string, { app: string; value: unknown }>;
+
+export type PermissionsMods = {
+	freeBusy: {
+		current: AccountACEInfo[];
+		new: AccountACEInfo;
+	};
+	inviteRight: {
+		current: AccountACEInfo[];
+		new: AccountACEInfo;
+	};
+};
+// TODO remove
+/**
+ * @deprecated
+ */
+export type CreateIdentityProps = {
+	requestId: number;
+	/** name of the identity */
+	zimbraPrefIdentityName: string | undefined;
+	/** personal part of email address put in from header */
+	zimbraPrefFromDisplay: string | undefined;
+	/** email address to put in from header.  Deprecated on data source as of bug 67068. */
+	zimbraPrefFromAddress: string | undefined;
+	/** Type of the email address from header. (sendAs or sendOnBehalfOf)  */
+	zimbraPrefFromAddressType: 'sendAs' | 'sendOnBehalfOf';
+	/** TRUE if we should set a reply-to header */
+	// TODO: update to boolean?
+	zimbraPrefReplyToEnabled: 'TRUE' | 'FALSE' | undefined;
+	/** personal part of email address put in reply-to header */
+	zimbraPrefReplyToDisplay: string | undefined;
+	/** address to put in reply-to header */
+	zimbraPrefReplyToAddress: string | undefined;
+	/** default mail signature for account/identity/dataSource */
+	zimbraPrefDefaultSignatureId: string | undefined;
+	/** forward/reply signature id for account/identity/dataSource */
+	zimbraPrefForwardReplySignatureId: string | undefined;
+	/** TRUE if we should look at zimbraPrefWhenSentToAddresses (deprecatedSince 5.0 in account) */
+	zimbraPrefWhenSentToEnabled: string | undefined;
+	/** TRUE if we should look at zimbraPrefWhenInFolderIds (deprecatedSince 5.0 in account) */
+	zimbraPrefWhenInFoldersEnabled: string | undefined;
+};
+
+export type CreateIdentityResponse = {
+	identity: [Identity];
+};
+export type ModifyIdentityResponse = Record<string, never>;
+export type DeleteIdentityResponse = Record<string, never>;
+export type ModifyPropertiesResponse = Record<string, never>;
+export type ModifyPrefsResponse = Record<string, never>;
+export type RevokeRightsResponse = {
+	ace?: AccountACEInfo[];
+};
+export type GrantRightsResponse = {
+	ace?: AccountACEInfo[];
+};
+
+export type IdentityMods = {
+	modifyList?: Record<string, { id: string; prefs: Partial<IdentityAttrs> }>;
+	deleteList?: string[];
+	createList?: { prefs: Partial<IdentityAttrs> }[];
+};
+
+export type PrefsMods = Record<string, unknown> & AccountSettingsPrefs;
+
+export interface Mods extends Record<string, Record<string, unknown>>, Partial<AccountSettings> {
+	props?: PropsMods;
+	prefs?: PrefsMods;
+	permissions?: PermissionsMods;
+	identity?: IdentityMods;
+}
+
+export type AddMod = <
+	ModsType extends keyof Mods = keyof Mods,
+	TypeKey extends keyof NonNullable<Mods[ModsType]> = keyof NonNullable<Mods[ModsType]>
+>(
+	type: ModsType,
+	key: TypeKey,
+	value: NonNullable<Mods[ModsType]>[TypeKey]
+) => void;
+
+export type RemoveMod = (type: keyof Mods, key: keyof NonNullable<Mods[typeof type]>) => void;
+
+export type Locale = {
+	id: string;
+	localName: string;
+	name: string;
+};
+export type AvailableLocalesResponse = {
+	locale: Array<Locale>;
+};
+
+export type NetworkState = SoapContext & {
+	noOpTimeout?: Timeout;
+	pollingInterval: number;
+	seq: number;
+};
+
+export type CreateTagRequest = {
+	tag: Omit<Tag, id>;
+	_jsns: string;
+};
+
+export type CreateTagResponse = {
+	tag: [Tag];
+};
+
+export type TagActionRequest = {
+	_jsns: string;
+	action: {
+		op: 'rename' | 'color' | 'delete' | 'update';
+		id: string;
+		name?: string;
+		color?: number;
+		rgb?: string;
+	};
+};
+export type TagActionResponse = {
+	action: { op: string; id: string };
+	_jsns: string;
+};
+
+export type ModifyPrefsRequest = {
+	_jsns: NameSpace;
+	_attrs: AccountSettingsPrefs;
+};
+
+export type CreateIdentityRequest = {
+	_jsns: NameSpace;
+	identity: {
+		name?: string;
+		_attrs: IdentityAttrs;
+	};
+};
+
+export type ModifyIdentityRequest = {
+	_jsns: NameSpace;
+	identity: {
+		_attrs?: Partial<IdentityAttrs>;
+	} & RequireAtLeastOne<Pick<Identity, 'id' | 'name'>>;
+};
+
+export type DeleteIdentityRequest = {
+	identity: { name?: string; id?: string };
+	_jsns: NameSpace;
+	requestId?: string;
+};
+
+export type ModifyPropertiesRequest = {
+	_jsns: NameSpace;
+	prop: Array<{ name: string; zimlet: string; _content: unknown }>;
+};
+
+export type BatchRequest = {
+	ModifyIdentityRequest?: Array<ModifyIdentityRequest>;
+	CreateIdentityRequest?: Array<CreateIdentityRequest>;
+	DeleteIdentityRequest?: Array<DeleteIdentityRequest>;
+	ModifyPrefsRequest?: ModifyPrefsRequest;
+	ModifyPropertiesRequest?: ModifyPropertiesRequest;
+	_jsns: NameSpace;
+};
+
+export type GetRightsRequest = {
+	ace?: Array<{ right: Right }>;
+	_jsns: NameSpace;
+};
+
+export type GetRightsResponse = {
+	ace?: Array<{
+		// Name or email address of the grantee.
+		// Not present if {grantee-type} is "all" or "pub"
+		d?: string;
+		// The type of grantee
+		gt: GranteeType;
+		right: Right;
+		// Type:String
+		// Zimbra ID of the grantee
+		zid?: string;
+	}>;
+};
+
+export type Right =
+	// Following rights are partial, they are the result of
+	// description: automatically add meeting invites from grantee to the target's calendar
+	// right type: preset
+	// target type(s): account
+	// grant target type: (default)
+	// right class: USER
+	| 'invite'
+	// description: login as another user.  Currently this is only honored for imap/pop3 login.
+	// right type: preset
+	// target type(s): account
+	// grant target type: (default)
+	// right class: USER
+	| 'loginAs'
+	// description: reply to messages in a shared folder as the owner of the folder
+	// right type: preset
+	// target type(s): account
+	// grant target type: account
+	// right class: USER
+	| 'sendAs'
+	// description: send messages on behalf of the grantor
+	// right type: preset
+	// target type(s): account
+	// grant target type: account
+	// right class: USER
+	| 'sendOnBehalfOf'
+	// description: view free/busy
+	// right type: preset
+	// target type(s): account
+	// grant target type: (default)
+	// right class: USER
+	| 'viewFreeBusy';
+
+export type NameSpace = 'urn:zimbraMail' | 'urn:zimbraAccount' | 'urn:zimbra';
+
+// The type of grantee:
+export type GranteeType =
+	// usr - Zimbra user
+	| 'usr'
+	// grp - Zimbra group(distribution list)
+	| 'grp'
+	// an external AD group
+	| 'egp'
+	// all - all authenticated users
+	| 'all'
+	// Zimbra Domain
+	| 'dom'
+	// non-Zimbra domain (used with sendToDistList right)
+	| 'edom'
+	// gst - non-Zimbra email address and password (not yet supported)
+	| 'gst'
+	// key - external user with an accesskey
+	| 'key'
+	// pub - public authenticated and unauthenticated access
+	| 'pub'
+	// Pseudo grantee type.  Granting code will map to usr/grp/egp or gst
+	| 'email';
